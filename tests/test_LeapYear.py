cfe6f4e01c40a30d474c5377e0b4bb02da1f9339
evjengit <111729179+evjengit@users.noreply.github.com>
2023-10-25 15:20:56 +0200

    sjekket inn filer fra oblig 2

---
+++ tests/test_LeapYear.py
@@ -0,0 +1,45 @@
+import pytest
+import sys
+sys.path.append('oblig_2')
+
+from app.leapYear import isLeapYear
+
+def test_leap_year_divisible_by_4_not_100():
+    # Arrange
+    year = 2008
+
+    # Act
+    result = isLeapYear(year)
+
+    # Assert
+    assert result == True
+
+def test_leap_year_divisible_by_400():
+    # Arrange
+    year = 1600
+
+    # Act
+    result = isLeapYear(year)
+
+    # Assert
+    assert result == True
+
+def test_non_leap_year_not_divisible_by_4():
+    # Arrange
+    year = 2001
+
+    # Act
+    result = isLeapYear(year)
+
+    # Assert
+    assert result == False
+
+def test_non_leap_year_divisible_by_100_not_400():
+    # Arrange
+    year = 1900
+
+    # Act
+    result = isLeapYear(year)
+
+    # Assert
+    assert result == False
